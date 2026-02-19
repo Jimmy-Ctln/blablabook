26dcf16b55f6e045cda1dd5b5520a005518a0875
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-02-20 00:16:23 +0100

    feat: Add possibility to see the detail page for one book when click on him

--- frontend/src/components/bookCardModal.tsx
+++ frontend/src/components/bookCardModal.tsx
@@ -1,15 +1,46 @@
 import type { BookDisplay } from "@/@types/books";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { Plus } from "lucide-react";
+import { Check, Plus } from "lucide-react";
+import { useNavigate } from "@tanstack/react-router";
+import { getUserBooks } from "@/api/books";
+import { useQuery } from "@tanstack/react-query";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { useAddBook } from "@/hooks/useAddBook";
+import { Loader } from "./Loader";
 
 type BookCardModalProps = {
   book: BookDisplay;
 };
 
 export default function BookCardModal({ book }: BookCardModalProps) {
+  const navigate = useNavigate();
+
+  const currentUser = useCurrentUser();
+  const userId = currentUser.data?.id;
+
+  const { data: userBooks = [] } = useQuery({
+    queryKey: ["userBooks", userId],
+    queryFn: () => getUserBooks(userId!),
+    enabled: !!userId,
+  });
+
+  // Navigate to internal book details page using the ISBN
+  const handleCardClick = (book: BookDisplay) => {
+    navigate({ to: `/books/${book.isbn}` });
+  };
+
+  // Check if a book result is already in the library
+  const isInLibrary = (book: BookDisplay) => {
+    if (!book.isbn?.length) return false;
+    return userBooks.some((b) => book.isbn?.includes(b.isbn));
+  };
+
+  // Mutation hook to add a book to the user's list
+  const addBook = useAddBook(userId);
+
   return (
-    <Card>
+    <Card className="cursor-pointer" onClick={() => handleCardClick(book)}>
       <CardContent className="flex justify-between items-center">
         <div className="w-full flex gap-2">
           {book.cover && (
@@ -25,9 +56,20 @@ export default function BookCardModal({ book }: BookCardModalProps) {
             <span>{book.author}</span>
           </div>
         </div>
-        <Button className="glass">
-          <Plus />
-        </Button>
+        {isInLibrary(book) ? (
+          <Check className="text-primary" />
+        ) : (
+          <Button
+            className="glass"
+            disabled={addBook.isPending}
+            onClick={(e) => {
+              e.stopPropagation();
+              addBook.mutate(book);
+            }}
+          >
+            {addBook.isPending ? <Loader text="" /> : <Plus />}
+          </Button>
+        )}
       </CardContent>
     </Card>
   );
