cb3537cd77217b3bcbd6175ede26edc749764c11
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-02-23 00:05:32 +0100

    feat(backend): Update dto

--- backend/src/user/dto/update-user.request.dto.ts
+++ backend/src/user/dto/update-user.request.dto.ts
@@ -31,18 +31,5 @@ export class UpdateUserRequestDto {
   @ApiPropertyOptional({ example: 'avatar1.png' })
   @IsString()
   @IsOptional()
-  @IsIn(
-    [
-      'image1.jpg',
-      'image2.jpg',
-      'image3.jpg',
-      'image4.jpg',
-      'image5.jpg',
-      'image6.jpg',
-    ],
-    {
-      message: 'Image must be one of the predefined images',
-    },
-  )
   image?: string;
 }
