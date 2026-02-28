f559642a1f3c4f3fad794560acbfdc26df4d07f1
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-02-28 11:10:14 +0100

    feat(frontend): Update bookDisplayMapper to standardize property names and include additional fields

--- frontend/src/lib/bookDisplayMapper.ts
+++ frontend/src/lib/bookDisplayMapper.ts
@@ -5,23 +5,32 @@ import type { BookDisplay } from "@/@types/books";
 // Maps a BookRow (internal) to a BookDisplay
 export function mapBookRowToDisplay(book: BookRow): BookDisplay {
   return {
-    key: book.id.toString(),
-    title: book.name,
+    id: book.id.toString(),
+    internalId: book.id, //Need to save original id of the bookRow
+    name: book.name,
     author: book.author,
     cover: book.coverId ?? "",
     isbn: book.isbn,
     categories: book.categories ?? [],
+    publishDate: book.publishedAt,
+    publisher: book.publishingHouse,
+    status: book.status,
+    readStart: book.readStart,
+    readEnd: book.readEnd,
+    addedAt: book.addedAt,
   };
 }
 
 // Maps an ExternalBook (external) to a BookDisplay
 export function mapExternalBookToDisplay(book: ExternalBook): BookDisplay {
   return {
-    key: book.key,
-    title: book.title,
+    id: book.key,
+    name: book.title,
     author: book.author,
     cover: book.cover ?? "",
     isbn: book.isbn,
     categories: book.categories ?? [],
+    publishDate: book.publishDate ?? "",
+    publisher: book.publisher ?? "",
   };
 }
