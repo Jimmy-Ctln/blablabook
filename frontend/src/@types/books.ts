86f800f49d5bf1e6a06cda35a865f26c5794a9dc
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-02-28 11:04:16 +0100

    feat(frontend): Update BookRow and BookDisplay interfaces for improved type consistency

--- frontend/src/@types/books.ts
+++ frontend/src/@types/books.ts
@@ -24,11 +24,11 @@ export interface BookRow {
   isbn: string;
   publishingHouse: string;
   publishedAt: string; // date format from Drizzle
+  status: BookStatus;
+  categories?: string[];
   readStart?: Date | null;
   readEnd?: Date | null;
   addedAt?: Date;
-  status?: BookStatus;
-  categories?: string[];
 }
 
 // /**
@@ -41,13 +41,18 @@ export interface BookRow {
 // BookDisplay: unified display type for the front end
 // Allows all components to use the same type, regardless of the source (internal or external)
 export interface BookDisplay {
-  key: string;
-  title: string;
+  id: string;
+  internalId?: number | undefined; // Only internal book
+  name: string;
   description?: string;
   author: string;
   cover: string;
   isbn: string;
   publisher: string;
   publishDate: string;
+  status?: string; //The status is optionnal because externals books don't have status
   categories: string[];
+  readStart?: Date | null;
+  readEnd?: Date | null;
+  addedAt?: Date;
 }
