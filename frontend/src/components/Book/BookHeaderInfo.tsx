2178ab4efc9c98daab65499ceef2bfa2db51a5c3
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2026-05-14 00:21:50 +0200

    feat: Improve the indentation of the title in the BookHeaderInfo component for better readability

--- frontend/src/components/Book/BookHeaderInfo.tsx
+++ frontend/src/components/Book/BookHeaderInfo.tsx
@@ -11,7 +11,9 @@ export const BookHeaderInfo: React.FC<BookHeaderInfoProps> = ({
   author,
 }) => (
   <div className="flex flex-col w-full items-center lg:items-start">
-    <h2 className="text-5xl md:text-5x text-foreground mb-2 text-center lg:text-left">{title}</h2>
+    <h2 className="text-5xl md:text-5x text-foreground mb-2 text-center lg:text-left">
+      {title}
+    </h2>
     <div className="text-lg text-foreground text-center lg:text-left">
       <span className="mr-2 text-muted-foreground">par</span>{" "}
       <span className="text-foreground">{author}</span>
